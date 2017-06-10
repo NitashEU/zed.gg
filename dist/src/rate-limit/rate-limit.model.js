8b978d9ac039d6a751056b3cc874a75c51d1d803
Ferhat Sayan <nitashsy@gmail.com>
2017-06-10 09:03:46 +0200

    Merge branch 'master' of https://github.com/NitashEU/zed.gg into release/0.0.1
    # Conflicts:
    #	.gitignore
    #	LICENSE
    #	README.md

--- dist/src/rate-limit/rate-limit.model.js
+++
@@ -1,7 +0,0 @@
-export class RateLimit {
-    constructor(seconds, maximumCalls) {
-        this.seconds = seconds;
-        this.maximumCalls = maximumCalls;
-    }
-}
-//# sourceMappingURL=rate-limit.model.js.map
--- src/rate-limit/rate-limit.ts
+++
@@ -1,9 +0,0 @@
-export class RateLimit {
-  private seconds: number;
-  private maximumCalls: number;
-
-  constructor(seconds: number, maximumCalls: number) {
-    this.seconds = seconds;
-    this.maximumCalls = maximumCalls;
-  }
-}
