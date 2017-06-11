bb19ed853520458802a3b22846f0dab8f4120623
Ferhat Sayan <nitashsy@gmail.com>
2017-06-11 11:25:12 +0200

    exported models in index

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export * from './dist/src/zed-gg';
 export * from './dist/src/enums/region.enum';
 export * from './dist/src/rate-limit/rate-limit';
+export * from './dist/src/models/summoner.model';
+export * from './dist/src/models/matchlist.model';
