f87ba4946ebfe826b084f988ef3e5916a440804d
Ferhat Sayan <nitashsy@gmail.com>
2017-06-11 11:24:28 +0200

    implemented endpoint for Matchlists byAccountId

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,7 +1,8 @@
+import { Matchlist } from './models/matchlist.model';
 import { Summoner } from './models/summoner.model';
 import { UrlAndConstructor } from './helpers/url-and-constructor';
 
-export namespace Endpoints {
+export namespace Endpoints { // TODO: Need to rework UrlAndConstructor creation
   export const baseUrl = 'https://{platformId}.api.riotgames.com/'
 
   export namespace Summoners {
@@ -11,5 +12,16 @@ export namespace Endpoints {
     export const byAccountId = new UrlAndConstructor(baseUrl + 'by-account/{accountId}', baseConstructor);
     export const bySummonerName = new UrlAndConstructor(baseUrl + 'by-name/{summonerName}', baseConstructor);
     export const bySummonerId = new UrlAndConstructor(baseUrl + '{summonerId}', baseConstructor);
+  }
+
+  export namespace Matches {
+    const baseUrl = 'lol/match/v3/matches/';
+  }
+
+  export namespace Matchlists {
+    const baseUrl = 'lol/match/v3/matchlists/';
+    const baseConstructor = Matchlist;
+
+    export const byAccountId = new UrlAndConstructor(baseUrl + 'by-account/{accountId}', baseConstructor);
   }
 }
