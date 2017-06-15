c26d2599ce8f17ae39a56220dc9d47bc8b994854
Ferhat Sayan <nitashsy@gmail.com>
2017-06-15 15:38:21 +0200

    some index.ts files

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,4 +2,5 @@ export * from './http-status-code.enum';
 export * from './queue.enum';
 export * from './region.enum';
 export * from './season.enum';
-export * from './map.enum';
+export * from './league-map.enum';
+export * from './tier.enum';
