09b8249209524f2088b9f18aaade40db4136fd39
Ferhat Sayan <nitashsy@gmail.com>
2017-06-21 17:41:50 +0200

    our index files in the root should be es5

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-export * from './dist/zed-gg';
-export * from './dist/rate-limit/rate-limit';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _zedGg = require('./dist/zed-gg');
+
+Object.keys(_zedGg).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _zedGg[key];
+    }
+  });
+});
+
+var _rateLimit = require('./dist/rate-limit/rate-limit');
+
+Object.keys(_rateLimit).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _rateLimit[key];
+    }
+  });
+});
--- request-options.js
+++ request-options.js
@@ -1,1 +1,17 @@
-export * from './dist/request-options/matchlist.options';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _matchlist = require('./dist/request-options');
+
+Object.keys(_matchlist).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _matchlist[key];
+    }
+  });
+});
