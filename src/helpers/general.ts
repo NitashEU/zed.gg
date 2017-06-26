8ce4d938ec13aaff565a3d7237dc6699c3c51c74
Ferhat Sayan <nitashsy@gmail.com>
2017-06-26 20:07:54 +0200

    We don't want asyncForEach in the main module

--- src/helpers/general.ts
+++ src/helpers/general.ts
@@ -1,5 +1,3 @@
-import * as async from 'async';
-
 export function isType(v: any, type: object | string): boolean {
   return typeof v === type;
 }
@@ -12,23 +10,4 @@ export function isPrimitiveOrPrimitiveClass(obj: any): boolean {
   return !!(['string', 'boolean', 'number'].indexOf((typeof obj)) > -1 || (obj instanceof String || obj === String ||
     obj instanceof Number || obj === Number ||
     obj instanceof Boolean || obj === Boolean));
-}
-
-export function asyncForEach<T>(arr: T[], fn: (v: T) => Promise<void>): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    async.forEach(arr, async (v, c) => {
-      try {
-        await fn(v);
-      }
-      catch (ex) {
-        throw ex;
-      }
-      finally {
-        c();
-      }
-    }, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  })
 }
